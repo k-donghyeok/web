e2dddb9cd81983a97415e138896cf3ca622ee698
K-donghyeok <rkdehdgur432@naver.com>
2024-06-12 18:36:12 +0900

    내정보 수정 페이지
    사진 크기 일정하도록 수정
    디자인 조금 수정

--- edit_info_scripts.js
+++ edit_info_scripts.js
@@ -1,6 +1,6 @@
 // Firebase SDK 추가
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-import { getFirestore, doc, setDoc, deleteField } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+import { getFirestore, doc, setDoc, deleteField, updateDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
 import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-storage.js";
 
